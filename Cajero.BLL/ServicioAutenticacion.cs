e443f41cfed5e5a5dcf2f8daa13a03a2642e81e9
Castillo423 <styvc432@gmail.com>
2026-09-16 19:51:01 -0600

    Bugs Corregidos y nuevos buggs añadidos

--- Cajero.BLL/ServicioAutenticacion.cs
+++ Cajero.BLL/ServicioAutenticacion.cs
@@ -15,7 +15,7 @@ namespace Cajero.BLL
         public string ValidarAcceso(string uidRfid, string pin)
         {
             // Administrador asignado mediante código para usar NFC
-            if (uidRfid == "0000002577992613")
+            if (uidRfid == "0000002577992612")
             {
                 if (pin == "0000") 
                     return "Administrador";
